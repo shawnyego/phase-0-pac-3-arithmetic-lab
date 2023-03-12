1ca0983fd1c07edb386b5eb72ee13e37faf76769
shawn <parvonisyego@gmail.com>
2023-03-12 19:57:47 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function Add(num1, num2){
+    return num1 + num2
+}
+function Subtract(num1, num2){
+    return num1 - num2
+}
+function Multiply(num1, num2){
+    return num1 * num2
+}
+function Divide(num1, num2){
+    return num1 / num2
+}
+function Increment(n){
+    return n += 1
+}
+function Decrement(n){
+    return n -= 1
+}
+function makeInt(n){
+    return parseInt(n, 10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
